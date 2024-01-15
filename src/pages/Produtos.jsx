7978b42dd579a181d3a03253b835cb901cec66fb
@Nicolas_Oliveira <125362779+Nicolas-tech25@users.noreply.github.com>
2024-01-15 11:11:59 -0300

    efeito colateral (loop)

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+
 function Produtos() {
+  /* O state "produtos" é inicializado como um array vazio. Posteriormente (após o carregamento dos dados da API) */
+  const [produtos, setProdutos] = useState([]);
+
+  const carregarDados = async () => {
+    try {
+      const resposta = await fetch(`https://fakestoreapi.com/products`);
+      const dados = await resposta.json();
+      console.log(dados);
+      setProdutos(dados);
+    } catch (error) {
+      console.log("Houve um erro: " + error);
+    }
+  };
+
+  carregarDados();
+
   return <h2>Produtos</h2>;
 }
 
